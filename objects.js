a5aa6547a516249737d4225967491d1536c82f3c
Nekwan Luke <contact@nekwanluke.com>
2019-04-05 16:01:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,7 +7,17 @@ function updatePlaylist (playlist, artistName, songTitle)
 
 
 
+ 
   describe('updatePlaylist(playlist, artistName, songTitle)', () => {
     before(() => {
       playlist['Slowdive'] = 'Alison'
       playlist['My Bloody Valentine'] = 'Sometimes'
+    })
+
+    it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
+      updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+
+      expect(playlist).
+        to.contain.all.keys({'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes', 'Phil Ochs': "Here's to the State of Mississippi"})
+    })
+  })
