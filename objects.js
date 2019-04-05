68db7de03435afabe2b2939b27f17f386c6f2438
Nekwan Luke <contact@nekwanluke.com>
2019-04-05 16:00:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,2 +1,13 @@
 var playlist = new Object({keys: '0'});
 
+function updatePlaylist (playlist, artistName, songTitle)
+{
+  return Object.assign
+}
+
+
+
+  describe('updatePlaylist(playlist, artistName, songTitle)', () => {
+    before(() => {
+      playlist['Slowdive'] = 'Alison'
+      playlist['My Bloody Valentine'] = 'Sometimes'
